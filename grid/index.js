de0794293778a04078b533038c75de6d85c42eff
Albert ten Napel <aptennap@gmail.com>
2026-06-10 12:27:59 +0200

    Add ToneJs

--- grid/index.js
+++ grid/index.js
@@ -1,7 +1,4 @@
 const container = document.getElementById("container");
-//container.addEventListener("pointermove", pointerMove);
-//container.addEventListener("pointerdown", pointerDown);
-//container.addEventListener("pointerup", pointerUp);
 
 let width = window.innerWidth;
 let height = window.innerHeight;
@@ -22,39 +19,12 @@ for (let i = 0; i < 128; i++) {
 function changeColor(i, color) {
   buttons[i].style.backgroundColor = color;
 }
-function resetButton(i) {
-  changeColor(i, "white");
-}
-function highlightButton(i) {
-  changeColor(i, "lightblue");
-}
 
-function calcIx(e, ox = 0, oy = 0) {
-  const x = Math.floor(((e.pageX + ox) / width) * 16);
-  const y = Math.floor(((e.pageY + oy) / height) * 8);
+function calcIx(e) {
+  const x = Math.min(15, Math.floor((e.pageX / width) * 16));
+  const y = Math.min(7, Math.floor((e.pageY / height) * 8));
   const i = y * 16 + x;
   return i;
-}
-
-function pointerMove(e) {
-  const i = calcIx(e);
-  const pi = calcIx(e, -e.movementX, -e.movementY);
-  if (e.buttons === 1) {
-    highlightButton(i);
-    if (pi !== i) resetButton(pi);
-  } else {
-    resetButton(i);
-  }
-}
-
-function pointerDown(e) {
-  const i = calcIx(e);
-  highlightButton(i);
-}
-
-function pointerUp(e) {
-  const i = calcIx(e);
-  resetButton(i);
 }
 
 const ongoingTouches = new Map();
@@ -64,65 +34,84 @@ for (let i = 0; i < 128; i++)
 
 function handleStart(e) {
   e.preventDefault();
-  for (const changedTouch of e.changedTouches) {
-    const id = changedTouch.identifier;
-    const i = calcIx(e);
-    ongoingTouches.set(id, i);
-    buttonTouches.get(i).add(id);
+  const id = e.pointerId;
+  container.setPointerCapture(id);
+  const i = calcIx(e);
+  ongoingTouches.set(id, i);
+  if (buttonTouches.get(i).size === 0)
     highlightButton(i);
-  }
+  buttonTouches.get(i).add(id);
 }
 function handleEnd(e) {
   e.preventDefault();
-  for (const changedTouch of e.changedTouches) {
-    const id = changedTouch.identifier;
-    if (!ongoingTouches.has(id)) {
-      console.error(`End: Could not find touch ${id}`);
-      continue;
-    }
-    const i = ongoingTouches.get(id);
-    ongoingTouches.delete(id);
-    buttonTouches.get(i).delete(id);
-    if (buttonTouches.get(i).size === 0)
-      resetButton(i);
+  const id = e.pointerId;
+  if (!ongoingTouches.has(id)) {
+    console.error(`End: Could not find touch ${id}`);
+    return;
   }
-}
-function handleCancel(e) {
-  e.preventDefault();
-  for (const changedTouch of e.changedTouches) {
-    const id = changedTouch.identifier;
-    if (!ongoingTouches.has(id)) {
-      console.error(`Cancel: Could not find touch ${id}`);
-      continue;
-    }
-    const i = ongoingTouches.get(id);
-    ongoingTouches.delete(id);
-    buttonTouches.get(i).delete(id);
-    if (buttonTouches.get(i).size === 0)
-      resetButton(i);
-  }
+  const i = ongoingTouches.get(id);
+  ongoingTouches.delete(id);
+  buttonTouches.get(i).delete(id);
+  if (buttonTouches.get(i).size === 0)
+    resetButton(i);
 }
 function handleMove(e) {
   e.preventDefault();
-  for (const changedTouch of e.changedTouches) {
-    const id = changedTouch.identifier;
-    if (!ongoingTouches.has(id)) {
-      console.error(`Move: Could not find touch ${id}`);
-      continue;
-    }
-    const pi = ongoingTouches.get(id);
-    const i = calcIx(e);
+  const id = e.pointerId;
+  if (!ongoingTouches.has(id))
+    return;
+  const pi = ongoingTouches.get(id);
+  const i = calcIx(e);
+  if (pi !== i) {
     ongoingTouches.set(id, i);
+    if (buttonTouches.get(i).size === 0)
+      highlightButton(i);
     buttonTouches.get(i).add(id);
-    if (pi !== i) {
-      buttonTouches.get(pi).delete(id);
-      if (buttonTouches.get(pi).size === 0)
-        resetButton(pi);
-    }
+
+    buttonTouches.get(pi).delete(id);
+    if (buttonTouches.get(pi).size === 0)
+      resetButton(pi);
   }
 }
 
-container.addEventListener("touchstart", handleStart);
-container.addEventListener("touchend", handleEnd);
-container.addEventListener("touchcancel", handleCancel);
-container.addEventListener("touchmove", handleMove);
+container.addEventListener("pointerdown", e => handleStart(e));
+container.addEventListener("pointerup", e => handleEnd(e));
+container.addEventListener("pointercancel", e => handleEnd(e));
+container.addEventListener("pointermove", e => handleMove(e));
+
+let synth;
+
+container.addEventListener("pointerdown", init);
+function init() {
+  Tone.setContext(new Tone.Context({ latencyHint: "interactive", lookAhead: 0 }));
+  Tone.start();
+  synth = new Tone.PolySynth(Tone.Synth).toDestination();
+  synth.maxPolyphony = 10;
+  container.removeEventListener("pointerdown", init);
+}
+
+function calcNote(i) {
+  const x = i % 16;
+  const y = 7 - Math.floor(i / 16);
+  return 60 - 24 + x + (y * 5);
+}
+function trigger(i) {
+  if (!synth) return;
+  const note = calcNote(i);
+  const freq = Tone.Frequency(note, "midi");
+  synth.triggerAttack(freq, Tone.now(), 0.8);
+}
+function release(i) {
+  const note = calcNote(i);
+  const freq = Tone.Frequency(note, "midi");
+  synth.triggerRelease(freq);
+}
+
+function resetButton(i) {
+  changeColor(i, "white");
+  release(i);
+}
+function highlightButton(i) {
+  changeColor(i, "lightblue");
+  trigger(i);
+}
